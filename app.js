88225fbc3d03377b742ab8381bb7189d442c9225
kasun Chandimal <39762853+kasunChandi@users.noreply.github.com>
2020-12-30 13:13:49 +0530

    creating welcome page

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 const port = 5000;
 const indexpage = require("./route/indexpage")
 const users = require("./route/user")
 
+//EJS 
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Routes 
 app.use('/', indexpage);
 app.use('/users', users);
 
